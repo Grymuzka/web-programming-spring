7f77116c71f08abc493d4cd2b06bd505c1e147da
Wiktoria Grymuza <w.grymuza@doittechnology.pl>
2025-01-30 00:59:11 +0100

    lab 9 task 1

--- src/main/java/com/example/webprogrammingspring/author/Author.java
+++ src/main/java/com/example/webprogrammingspring/author/Author.java
@@ -2,10 +2,17 @@ package com.example.webprogrammingspring.author;
 
 import com.example.webprogrammingspring.book.Book;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.util.List;
 
 
@@ -13,12 +20,22 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Imię i nazwisko nie może być puste")
+    @Size(min = 2, max = 100, message = "Imię i nazwisko musi mieć od 2 do 100 znaków")
     private String name;
+
+    @DateTimeFormat
+    @Past(message = "Data urodzenia musi być w przeszłości")
+    private LocalDate birthDate;
+
+    @Positive(message = "Wzrost nie może być ujemny")
+    private float height;
 
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Book> books;
--- src/main/java/com/example/webprogrammingspring/book/Book.java
+++ src/main/java/com/example/webprogrammingspring/book/Book.java
@@ -2,6 +2,12 @@ package com.example.webprogrammingspring.book;
 
 import com.example.webprogrammingspring.author.Author;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,15 +16,23 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Tytuł nie może być pusty")
+    @Size(min = 1, max = 255, message = "Tytuł musi mieć od 1 do 255 znaków")
     private String title;
+
+    @Min(value = 1900, message = "Rok publikacji musi być większy niż 1900")
+    private int publishYear;
 
     @ManyToOne
     @JoinColumn(name = "author_id")
+    @NotNull(message = "Autor nie może być null")
+    @Valid
     private Author author;
 }
