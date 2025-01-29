3358ceae829f21340c7e5594d32d3cad836180f0
Wiktoria Grymuza <w.grymuza@doittechnology.pl>
2025-01-29 22:46:38 +0100

    add log info

--- src/main/java/com/example/webprogrammingspring/service/CleanupScheduler.java
+++ src/main/java/com/example/webprogrammingspring/service/CleanupScheduler.java
@@ -1,14 +1,16 @@
-package com.example.webprogrammingspring;
+package com.example.webprogrammingspring.service;
 
 import com.example.webprogrammingspring.entity.Order;
 import com.example.webprogrammingspring.repository.OrderRepository;
 import com.example.webprogrammingspring.type.OrderStatus;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CleanupScheduler {
@@ -17,10 +19,12 @@ public class CleanupScheduler {
 
     @Scheduled(cron = "0 */3 * * * ?")
     public void cleanupOrders() {
-
+        log.info(">> Started order cleanup process");
         List<Order> draftOrders = orderRepository.findAll().stream()
                 .filter(order -> order.getStatus().equals(OrderStatus.DRAFT))
                 .toList();
         orderRepository.deleteAll(draftOrders);
+        log.info("<< Finished order cleanup process - cleaned up {} orders", draftOrders.size());
+
     }
 }
